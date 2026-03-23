475a9020f91827b5557150915c39593e6f1c4c71
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-23 17:39:17 -0300

    Python

---
+++ atividades do 01 ao 27/atvd 018.py
@@ -0,0 +1,11 @@
+#PA (progressão aritmética) v2
+pri = int(input('Primeiro termo: '))
+raz = int(input('Razão da PA: '))
+ter = pri
+cont = 1
+
+while cont <= 10:
+    print(f'{ter} → ', end='')
+    term += raz
+    cont += 1
+print('Final.')
